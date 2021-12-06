185f50e6cde0af4bd8f8778f0ffb545ef266df0f
Stefan Fuchs <stefan@fuchs.pub>
2021-12-06 22:34:26 +0100

    Aufgabe 01

---
+++ aufgabe-01/aufgabe-01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace aufgabe_01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo!\r\n");
+            Console.Write("Bitte gib ein beliebiges Wort ein und bestätige mit Enter: ");
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+            Console.ReadKey();
+        }
+    }
+}
