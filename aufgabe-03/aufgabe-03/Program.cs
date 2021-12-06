19582384a0e3419416cc1b65d6f479cdc2d408b9
Stefan Fuchs <stefan@fuchs.pub>
2021-12-06 22:51:15 +0100

    Aufgabe 03

---
+++ aufgabe-03/aufgabe-03/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace aufgabe_03
+{
+    internal class Program
+    {
+
+        static ConsoleKey[] breakkeys = new ConsoleKey[]
+        {
+            ConsoleKey.Enter,
+            ConsoleKey.Spacebar,
+            ConsoleKey.Tab
+        };
+        static char[] breakchars = new char[]
+        {
+            '?', '!', '.', ',', ';', ':', '!', '?'
+        };
+
+        static string expectedWord = "Welt";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo!\r\n");
+            Console.WriteLine("Bitte gib folgendes Wort ein und bestätige mit Enter: " + expectedWord);
+            string word = "";
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            while (!breakkeys.Contains(keyInfo.Key) && !breakchars.Contains(keyInfo.KeyChar))
+            {
+                word += keyInfo.KeyChar;
+                keyInfo = Console.ReadKey();
+            }
+            Console.WriteLine();
+            Console.Write("Deine Eingabe: " + word + " - ");
+            if (word == expectedWord)
+            {
+                Console.WriteLine("Das ist korrekt!");
+            } else
+            {
+                Console.WriteLine("Das ist falsch!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
