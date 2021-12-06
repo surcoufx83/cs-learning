376df85c747af0ee473c8e306d9211c431675449
Stefan Fuchs <stefan@fuchs.pub>
2021-12-06 22:46:25 +0100

    Aufgabe 02

---
+++ aufgabe-02/aufgabe-02/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+
+namespace aufgabe_02
+{
+    internal class Program
+    {
+
+        static ConsoleKey[] breakkeys = new ConsoleKey[]
+        {
+            ConsoleKey.Enter,
+            ConsoleKey.Spacebar,
+            ConsoleKey.Tab
+        };
+        static char[] breakchars = new char[]
+        {
+            '?', '!', '.', ',', ';', ':', '!', '?'
+        };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hallo!\r\n");
+            Console.Write("Bitte gib ein beliebiges Wort ein und bestätige mit Enter: ");
+            string word = "";
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            while (!breakkeys.Contains(keyInfo.Key) && !breakchars.Contains(keyInfo.KeyChar))
+            {
+                word += keyInfo.KeyChar;
+                keyInfo = Console.ReadKey();
+            }
+            Console.WriteLine("\r\n" + word);
+            Console.ReadKey();
+        }
+    }
+}
