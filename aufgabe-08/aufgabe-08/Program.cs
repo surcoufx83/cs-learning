86296d1118aab2e980c58ce61fd1341e58fe2993
Stefan Fuchs <stefan@fuchs.pub>
2021-12-07 20:58:56 +0100

    Aufgabe 08

---
+++ aufgabe-08/aufgabe-08/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Linq;
+
+namespace aufgabe_08
+{
+    internal class Program
+    {
+
+        static Random random;
+        static ConsoleKey[] breakkeys = new ConsoleKey[]
+        {
+            ConsoleKey.Enter,
+            ConsoleKey.Spacebar,
+            ConsoleKey.Tab
+        };
+        static char[] breakchars = new char[]
+        {
+            '?', '!', '.', ',', ';', ':', '!', '?'
+        };
+        static string[] wordlist = new string[]
+        {
+            "Eine", "wunderbare", "Heiterkeit", "hat", "meine", "ganze", "Seele", "eingenommen", "gleich", "den",
+            "süßen", "Frühlingsmorgen", "die", "ich", "mit", "ganzem", "Herzen", "genieße", "Ich", "bin", "allein",
+            "und", "freue", "mich", "meines", "Lebens", "in", "dieser", "Gegend", "die", "für", "solche", "Seelen",
+            "geschaffen", "ist", "wie", "die", "meine", "Ich", "bin", "so", "glücklich", "mein", "Bester", "so",
+            "ganz", "in", "dem", "Gefühle", "von", "ruhigem", "Dasein", "versunken", "daß", "meine", "Kunst",
+            "darunter", "leidet", "Ich", "könnte", "jetzt", "nicht", "zeichnen", "nicht", "einen", "Strich", "und",
+            "bin", "nie", "ein", "größerer", "Maler", "gewesen", "als", "in", "diesen", "Augenblicken", "Wenn",
+            "das", "liebe", "Tal", "um", "mich", "dampft", "und", "die", "hohe", "Sonne", "an", "der", "Oberfläche",
+            "der", "undurchdringlichen", "Finsternis", "meines", "Waldes", "ruht", "und", "nur", "einzelne", "Strahlen"
+        };
+
+        static string expectedWord;
+
+        static void Main(string[] args)
+        {
+            random = new Random();
+            Console.WriteLine("Hallo!\r\n");
+            while (true)
+            {
+                Console.WriteLine("Neues Spiel, los geht's ...");
+                int wordcount = 0;
+                int totalpoints = 0;
+                while (wordcount < 10)
+                {
+                    int points = cycle(wordcount + 1);
+                    if (points == -1)
+                        return;
+                    totalpoints += points;
+                    wordcount++;
+                }
+                Console.WriteLine("Geschafft!");
+                Console.WriteLine("Du hast " + totalpoints.ToString() + " Punkte erreicht!");
+                Console.WriteLine("Beliebige Taste für nächste Runde...\r\n\r\n");
+
+                Console.ReadKey();
+            }
+        }
+
+        static int cycle(int counter)
+        {
+            expectedWord = wordlist[random.Next(wordlist.Length)];
+            Console.WriteLine("[" + counter.ToString().PadLeft(2) + "/10] Bitte gib folgendes Wort ein: " + expectedWord);
+            string word = "";
+            ConsoleKeyInfo keyInfo = Console.ReadKey();
+            DateTime start = DateTime.Now;
+            while (!breakkeys.Contains(keyInfo.Key) && !breakchars.Contains(keyInfo.KeyChar))
+            {
+                if (keyInfo.Key == ConsoleKey.Backspace)
+                {
+                    Console.Write(" \b");
+                    word = word.Substring(0, word.Length - 1);
+                }
+                else
+                    word += keyInfo.KeyChar;
+                keyInfo = Console.ReadKey();
+            }
+            DateTime end = DateTime.Now;
+            TimeSpan timeSpan = end - start;
+
+            int falseLetters = 0;
+            for (int i = 0; i < Math.Min(word.Length, expectedWord.Length); i++)
+            {
+                if (word[i] != expectedWord[i])
+                    falseLetters++;
+            }
+            falseLetters += Math.Abs(expectedWord.Length - word.Length);
+
+            int points = Math.Max(0, 100 - (int)((timeSpan.TotalSeconds - (double)expectedWord.Length) * 15) - (falseLetters * 35));
+
+            Console.WriteLine();
+            Console.Write("Deine Eingabe: " + word + " - ");
+            if (word.ToLower() == "quit" || word == "")
+            {
+                return -1;
+            }
+            else if (word == expectedWord)
+            {
+                Console.WriteLine("Das ist korrekt!");
+            }
+            else
+            {
+                Console.WriteLine("Das ist falsch!");
+            }
+            Console.WriteLine();
+            return points;
+        }
+
+    }
+}
